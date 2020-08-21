729b60a4e8e7a686d96841f80c2120cd1f8724d4
Sergio Alegre <sergio.alegre.arribas@gmail.com>
2020-08-21 10:16:16 +0200

    Update ejemplo_file-null-resource.tf

--- Curso_Udemy_2020/12 - Provisioners/ejemplo_file-null-resource.tf
+++ Curso_Udemy_2020/12 - Provisioners/ejemplo_file-null-resource.tf
@@ -1,6 +1,5 @@
-#Basado en: https://www.udemy.com/course/terraform-indepth-with-7-realtime-casestudies/learn/lecture/11073130#questions
-
 #Ejemplo de file provision y null resource: copiaremos un fichero a una instacia ya creada. No la vamos a crear
+#Basado en: https://www.udemy.com/course/terraform-indepth-with-7-realtime-casestudies/learn/lecture/11073130#questions
 
 resource "null_resource" "cluster" {
   provisioner "file" {
@@ -14,3 +13,14 @@ resource "null_resource" "cluster" {
     }
   }
 }
+
+
+
+#ejemplo de file provisioner copiando contenido en lugar de fichero
+#Basado en: https://www.udemy.com/course/terraform-indepth-with-7-realtime-casestudies/learn/lecture/11176428#questions
+
+resource "null_resource" "cluster" {
+  provisioner "file" {
+    content = "esto es lo que escribiremos en el archivo de destino"
+    destination = "/tmp/archivo.log"
+    ...
